7a4f1d6b8abc14e29e4226e1817f9b66d1b30b9e
valentinvladimir <valypetre@gmail.com>
2019-12-12 22:37:37 +0100

    added 11_VPC_Project_Refractor

--- 11_VPC_Project_Refractor/ec2.tf
+++ 11_VPC_Project_Refractor/ec2.tf
@@ -1,21 +1,21 @@
 resource "aws_instance" "PublicEC2" {
-  ami                    = "${var.ami}"
-  instance_type          = "${var.instance_type}"
+  ami                    = var.ami
+  instance_type          = var.instance_type
   vpc_security_group_ids = [aws_security_group.allow_ssh.id]
   subnet_id              = aws_subnet.PublicSubnet_A.id
-  key_name               = "ec2_keypair"
+  key_name               = "1"
   tags = {
     Name = "PublicEC2"
   }
-  depends_on = ["aws_vpc.mainvpc", "aws_subnet.PublicSubnet_A", "aws_security_group.allow_ssh"]
+  depends_on = [aws_vpc.mainvpc, aws_subnet.PublicSubnet_A, aws_security_group.allow_ssh]
 }
 
 resource "aws_instance" "PrivateEC2" {
-  ami                    = "${var.ami}"
-  instance_type          = "${var.instance_type}"
+  ami                    = var.ami
+  instance_type          = var.instance_type
   vpc_security_group_ids = [aws_security_group.allow_ssh.id]
   subnet_id              = aws_subnet.PrivateSubnet_A.id
-  key_name               = "ec2_keypair"
+  key_name               = "1"
   tags = {
     Name = "PrivateEC2"
   }
--- 11_VPC_Project_Refractor/var.tf
+++ 11_VPC_Project_Refractor/var.tf
@@ -1,9 +1,9 @@
 variable "region" {
-
+  default = "eu-central-1"
 }
 
 variable "vpc_cidr" {
-
+  default = "10.0.0.0/16"
 }
 
 variable "instance_type" {
@@ -11,18 +11,18 @@ variable "instance_type" {
 }
 
 variable "ami" {
-  default = "ami-0b898040803850657"
-
+#  default = "ami-0b898040803850657"
+  default = "ami-0062c497b55437b01"
 }
 
 variable "public_subnet_cidr" {
-
+  default = "10.0.0.0/24"
 }
 
 variable "private_subnet_cidr" {
-
+  default = "10.0.16.0/20"
 }
 
 variable "availability_zone" {
-
+  default = "eu-central-1c"
 }
